4e057e89f9eb34c467fb14826351997e237d652c
pi665534 <pi665534@gmail.com>
2020-03-09 18:48:41 +0300

    init

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => (
+        <div className="jumbotron">
+            <div className="container">
+                <h1 className="display-4">Одно из лучших React приложений</h1>
+                <p className="lead">
+                    Версия приложения <strong>1.1.50</strong>
+                </p>
+            </div>
+        </div>
+    )
